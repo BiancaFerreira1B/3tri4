7e0171b6057248dcf70909a8b4331a1381dc2b3a
bianca2B <143563325+BiancaFerreira1B@users.noreply.github.com>
2024-10-18 19:42:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Zootecnia',
+    'É igual a veterinária?',
+    'Não. O zootecnista previne e o veterinário trata.'
+)
+
+criaCartao(
+    'Agronimia',
+    'Lida só em campo?',
+    'Não. Tem aulas práticas e teóricas.'
+)
+
+criaCartao(
+    'Zootecnia',
+    'Qual a finalidade da zootecnia?',
+    'Promover e aplicar medidas de fomento à produção animal.'
+)
+
